7fdb21c947abe5bfd218b00d728cad7ecd450478
nishakumari891247 <nishakumari891247@gmail.com>
2026-06-25 14:11:56 +0530

    programs

---
+++ Day25.c/Q99.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char names[5][50];
+    char temp[50];
+    int i, j;
+
+    printf("Enter 5 names:\n");
+
+    for(i = 0; i < 5; i++) {
+        scanf("%s", names[i]);
+    }
+
+    // Sorting names alphabetically
+    for(i = 0; i < 4; i++) {
+        for(j = i + 1; j < 5; j++) {
+            if(strcmp(names[i], names[j]) > 0) {
+                strcpy(temp, names[i]);
+                strcpy(names[i], names[j]);
+                strcpy(names[j], temp);
+            }
+        }
+    }
+
+    printf("\nNames in alphabetical order:\n");
+
+    for(i = 0; i < 5; i++) {
+        printf("%s\n", names[i]);
+    }
+
+    return 0;
+}
